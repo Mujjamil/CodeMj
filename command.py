dbba8cb5f096a3e8c9b8c05bcd1dcb35695bf9d5
Mujjamil <74098464+Mujjamil@users.noreply.github.com>
2024-09-17 19:21:06 +0530

    Adding commands in file

--- command.py
+++ command.py
@@ -3,3 +3,4 @@
 #git status = to check the current state of your project
 #git add =  to add all files you want to commit into a staging area, which can be seen as a temporary repository. This command will also stage any changed associated with tracked files.
 #git commit -m "message" = this is where we actually save our changes in the history of our project. The message that we use here is called a commit message and it's what you'll see when you run git log.
+#git push origin main = to upload your local commits to the remote repository on GitHub.

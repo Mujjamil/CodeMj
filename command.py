1d3c584146cc3103819eeebcee8de367e9e6da13
Mujjamil <74098464+Mujjamil@users.noreply.github.com>
2024-09-18 10:49:18 +0530

    Adding command file

--- command.py
+++ command.py
@@ -1,6 +1,13 @@
 #ls = listing folder
-#cs = is for opening that folder in terminal
+#cd = is for opening that folder in terminal
 #git status = to check the current state of your project
 #git add =  to add all files you want to commit into a staging area, which can be seen as a temporary repository. This command will also stage any changed associated with tracked files.
 #git commit -m "message" = this is where we actually save our changes in the history of our project. The message that we use here is called a commit message and it's what you'll see when you run git log.
 #git push origin main = to upload your local commits to the remote repository on GitHub.
+#cd .. = to go back one folder
+# mkdir = create new directory
+#git init = initialize a Git repository in the current folder.
+#git remote add origin = to set up a connection between our local repository and the remote repository that we've created on GitHub.
+#git remote -v = to list all of your configured remotes.
+#git branch = shows branch name
+# git branch -N <branch_name> = create new branch with given name
